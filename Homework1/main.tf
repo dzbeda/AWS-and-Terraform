5030e933edac60309f17b54942422791830feaca
Zbeda, Dudu <Dudu.Zbeda@cognyte.com>
2022-02-20 16:08:09 +0200

    Userdata

--- Homework1/main.tf
+++ Homework1/main.tf
@@ -12,45 +12,49 @@ terraform {
 # configure provider
 provider "aws" {
   region     = "us-east-1"
-  access_key = ""
-  secret_key = ""
+  access_key = var.aws_key
+  secret_key = var.aws_secret
 }
 
 resource "aws_ebs_volume" "nginx_disk" {
-  count = 2
+  count             = 2
   availability_zone = "us-east-1a" #Do i need to spcifiy it in both ec2 and volume resources?
   size              = 10
-  encrypted = "true"
-  type = "gp2"
+  encrypted         = "true"
+  type              = "gp2"
 
   tags = {
-    Name = join("nginx volume",[count.index])
-    purpose ="web site server"
+    Name    = "nginx-volume-${count.index + 1}"
+    purpose = "web site server"
   }
 }
 
 resource "aws_instance" "nginx_server" {
-  count = 2
-  ami           = "ami-04505e74c0741db8d"  #This is ubuntu 20 ami
-  instance_type = "t2.micro"
+  count             = 2
+  ami               = "ami-04505e74c0741db8d" #This is ubuntu 20 ami
+  instance_type     = "t2.micro"
   availability_zone = "us-east-1a"
-  user_data = <<EOF
-#! /bin/bash
-sudo apt-get update
-sudo apt-get install -y nginx
-echo "<h1>Welcome to Grandpa's Whiskey</h1>" | sudo tee /var/www/html/index.html
-sudo systemctl start nginx
-sudo systemctl enable nginx
-EOF
+  user_data = templatefile("./userdata.tpl", {
+    server_id = count.index + 1
+    }
+  )
+  #<<EOF
+  # #! /bin/bash
+  # sudo apt-get update
+  # sudo apt-get install -y nginx
+  # echo "<h1>Welcome to Grandpa's Whiskey</h1>" | sudo tee /var/www/html/index.html
+  # sudo systemctl start nginx
+  # sudo systemctl enable nginx
+  #EOF
   tags = {
-    Name = join("nginx server ",[count.index])
-    owner = "dudu"
-    purpose ="web site server"
+    Name    = "nginx-server-${count.index + 1}"
+    owner   = "dudu"
+    purpose = "web site server"
   }
 }
 
 resource "aws_volume_attachment" "nginx_volume" {
-  count = 2
+  count       = 2
   device_name = "/dev/sdh"
   volume_id   = aws_ebs_volume.nginx_disk.*.id[count.index]
   instance_id = aws_instance.nginx_server.*.id[count.index]
